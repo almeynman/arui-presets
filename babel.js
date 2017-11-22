4ef39399dfd235266944da60e32ea2939b72cf3e
Alexandr Kitov <akitov@alfabank.ru>
2017-11-22 19:30:17 +0300

    feat(babel): allow to pass module option

--- babel.js
+++ babel.js
@@ -1,33 +1,45 @@
 /* eslint global-require: 0 */
-module.exports = {
-    presets: [
-        [
-            'env',
-            {
-                targets: {
-                    browsers: require('./supporting-browsers'),
-                    node: 'current'
-                },
-                loose: true
-            }
-        ],
-        'react'
-    ],
-    plugins: [
-        'babel-plugin-transform-proto-to-assign',
-        'babel-plugin-transform-decorators-legacy',
-        'babel-plugin-transform-class-properties',
-        'babel-plugin-transform-export-extensions',
-        ['babel-plugin-transform-object-rest-spread', { useBuiltIns: true }],
-        ['babel-plugin-transform-runtime', { polyfill: false, helpers: false }]
-    ],
-    env: {
-        production: {
-            plugins: [
-                'babel-plugin-transform-react-remove-prop-types',
-                'babel-plugin-transform-react-constant-elements',
-                'babel-plugin-transform-react-inline-elements'
-            ]
-        }
+
+function buildPresets(context, options) {
+    let modules = 'commonjs';
+    if (options && typeof options.modules !== 'undefined') {
+        modules = options.modules;
     }
-};
+
+    return {
+        presets: [
+            [
+                'env',
+                {
+                    targets: {
+                        browsers: require('./supporting-browsers'),
+                        node: 'current'
+                    },
+                    loose: true,
+                    modules
+                }
+            ],
+            'react'
+        ],
+        plugins: [
+            'babel-plugin-transform-proto-to-assign',
+            'babel-plugin-transform-decorators-legacy',
+            'babel-plugin-transform-class-properties',
+            'babel-plugin-transform-export-extensions',
+            ['babel-plugin-transform-object-rest-spread', { useBuiltIns: true }],
+            ['babel-plugin-transform-runtime', { polyfill: false, helpers: false }]
+        ],
+        env: {
+            production: {
+                plugins: [
+                    'babel-plugin-transform-react-remove-prop-types',
+                    'babel-plugin-transform-react-constant-elements',
+                    'babel-plugin-transform-react-inline-elements'
+                ]
+            }
+        }
+    };
+}
+
+
+module.exports = buildPresets;
